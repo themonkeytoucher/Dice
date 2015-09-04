f401307c2a2f6261df86f3418e5c4d3a60d9dccf
themonkeytoucher <mysterysteel@gmail.com>
2015-09-04 08:35:30 -0700

    multiple dice made

--- Dice.pde
+++ Dice.pde
@@ -2,25 +2,34 @@ Die dice;
 
 void setup()
 {	
-	size(300,300);
+	size(320,360);
 	noLoop();
 }
 void draw()
 {
 	background(255);
-	dice = new Die(150,150);
-	dice.show();
-	dice.roll();
-		//your code here
+
+	for (int i=0; i<300; i=i+40){
+
+		for (int y=0; y<300; y=y+40){
+			dice = new Die(i,y);
+			dice.roll();
+			dice.show();
+		}
+	}
+	if (mousePressed == true && mouseButton == RIGHT) {
+			noLoop();
+	}
+			//your code here
 }
-void mouseClicked()
+void mousePressed()
 {
-	redraw();
+	loop();
 }
 
 class Die //models one single dice cube
 {
-	int myX,myY,rollr,answ;
+	int myX,myY,rollr;
 	
 	//variable declarations here
 	Die(int x, int y)
@@ -28,52 +37,50 @@ class Die //models one single dice cube
 		//variable initializations here
 		myX = x;
 		myY = y;
-		rollr = 6;
-		answ = 1;
+		rollr = 0;
 
 	}
+
 	void roll()
 	{
 		//your code here
 		rollr = (int)(Math.random()*6)+1;
-								
-
 
 	}
+
 	void show()
 	{	
 		fill(180,5,121);
 		rect (myX,myY,40,40);
 		fill(0);
-		strokeWeight(5);
 
 		if (rollr==1) {
-			point(myX+20, myY+20);
+			ellipse(myX+20, myY+20,5,5);
 		} else if (rollr == 2) {
-			point (myX+10,myY+10);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		} else if (rollr == 3) {
-			point (myX+10,myY+10);
-			point (myX+20,myY+20);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+20,myY+20,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		} else if (rollr == 4) {
-			point (myX+10,myY+10);
-			point (myX+30,myY+10);
-			point (myX+10,myY+30);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+10,5,5);
+			ellipse (myX+10,myY+30,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		} else if (rollr == 5){
-			point (myX+10,myY+10);
-			point (myX+30,myY+10);
-			point (myX+10,myY+30);
-			point (myX+30,myY+30);
-			point (myX+20,myY+20);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+10,5,5);
+			ellipse (myX+10,myY+30,5,5);
+			ellipse (myX+30,myY+30,5,5);
+			ellipse (myX+20,myY+20,5,5);
 		} else if (rollr == 6) {
-			point (myX+10,myY+10);
-			point (myX+30,myY+10);
-			point (myX+10,myY+20);
-			point (myX+30,myY+20);
-			point (myX+10,myY+30);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+10,5,5);
+			ellipse (myX+10,myY+20,5,5);
+			ellipse (myX+30,myY+20,5,5);
+			ellipse (myX+10,myY+30,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		}
 
 	}
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -18,25 +18,34 @@ Die dice;
 
 public void setup()
 {	
-	size(300,300);
+	size(320,360);
 	noLoop();
 }
 public void draw()
 {
 	background(255);
-	dice = new Die(150,150);
-	dice.show();
-	dice.roll();
-		//your code here
+
+	for (int i=0; i<300; i=i+40){
+
+		for (int y=0; y<300; y=y+40){
+			dice = new Die(i,y);
+			dice.roll();
+			dice.show();
+		}
+	}
+	if (mousePressed == true && mouseButton == RIGHT) {
+			noLoop();
+	}
+			//your code here
 }
-public void mouseClicked()
+public void mousePressed()
 {
-	redraw();
+	loop();
 }
 
 class Die //models one single dice cube
 {
-	int myX,myY,rollr,answ;
+	int myX,myY,rollr;
 	
 	//variable declarations here
 	Die(int x, int y)
@@ -44,52 +53,50 @@ class Die //models one single dice cube
 		//variable initializations here
 		myX = x;
 		myY = y;
-		rollr = 6;
-		answ = 1;
+		rollr = 0;
 
 	}
+
 	public void roll()
 	{
 		//your code here
 		rollr = (int)(Math.random()*6)+1;
-								
-
 
 	}
+
 	public void show()
 	{	
 		fill(180,5,121);
 		rect (myX,myY,40,40);
 		fill(0);
-		strokeWeight(5);
 
 		if (rollr==1) {
-			point(myX+20, myY+20);
+			ellipse(myX+20, myY+20,5,5);
 		} else if (rollr == 2) {
-			point (myX+10,myY+10);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		} else if (rollr == 3) {
-			point (myX+10,myY+10);
-			point (myX+20,myY+20);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+20,myY+20,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		} else if (rollr == 4) {
-			point (myX+10,myY+10);
-			point (myX+30,myY+10);
-			point (myX+10,myY+30);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+10,5,5);
+			ellipse (myX+10,myY+30,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		} else if (rollr == 5){
-			point (myX+10,myY+10);
-			point (myX+30,myY+10);
-			point (myX+10,myY+30);
-			point (myX+30,myY+30);
-			point (myX+20,myY+20);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+10,5,5);
+			ellipse (myX+10,myY+30,5,5);
+			ellipse (myX+30,myY+30,5,5);
+			ellipse (myX+20,myY+20,5,5);
 		} else if (rollr == 6) {
-			point (myX+10,myY+10);
-			point (myX+30,myY+10);
-			point (myX+10,myY+20);
-			point (myX+30,myY+20);
-			point (myX+10,myY+30);
-			point (myX+30,myY+30);
+			ellipse (myX+10,myY+10,5,5);
+			ellipse (myX+30,myY+10,5,5);
+			ellipse (myX+10,myY+20,5,5);
+			ellipse (myX+30,myY+20,5,5);
+			ellipse (myX+10,myY+30,5,5);
+			ellipse (myX+30,myY+30,5,5);
 		}
 
 	}
